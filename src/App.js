bdcf086a6cbdc35ab9a8ca52e730cd79410cc04a
juliatk <juia_atkeson25@milton.edu>
2025-04-08 09:23:23 -0400

    in class april 8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-export default function Square(){
+import './index.css';
+import { useState } from 'react';
+
+
+function Square() {
+  
+    console.log('clicked!');
+    const [value, setValue] = useState(null);
+  return <button className="square">{value}</button>;
+  
+  function handleClick() {
+    setValue('X');
+}
+
+
+return (
+  <button
+    className="square"
+    onClick={handleClick}
+  >
+    {value}
+  </button>
+);
+}
+
+export default function Board(){
   return(
 <>
-  return <button classname= "square">X</button>;
-  return <button classname= "square">X</button>;
+<div className = "board-row">
+<Square  />
+        <Square />
+        <Square />
+ </div>
+
+ <div className = "board-row">
+ <Square  />
+        <Square />
+        <Square />
+ </div>
+
+ <div className = "board-row">
+ <Square  />
+        <Square />
+        <Square />
+ </div>
+
   </>
 
   );
+
+  
 }
